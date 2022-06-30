81d8c86e854cf4717c21d816004dc0f859d0558f
gturner <genevieve.turner@anu.edu.au>
2022-06-30 16:16:13 +1000

    Updated anu theme to 7.3.

--- src/themes/anu/entry-components.ts
+++
@@ -1,7 +0,0 @@
-import { PublicationComponent } from './app/item-page/simple/item-types/publication/publication.component';
-import { PersonComponent } from './app/item-page/simple/item-types/person/person.component';
-
-export const ENTRY_COMPONENTS = [
-  PublicationComponent
-  , PersonComponent
-];
