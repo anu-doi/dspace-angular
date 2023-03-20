f3bdc989249103ab9cce28a31c973e71c4f3ec77
tsakshay <37055507+tsakshay@users.noreply.github.com>
2023-03-21 10:25:39 +1100

    Anu dspace74 akshay (#2)
    * Change footer, search in different pages. Update lazy theme module and i18n en module.
    
    * Update gitignore and lazy theme module
    
    * Update navbar component related links and lazy theme module
    
    * Update navbar, comm page comp and footer removing trailing and double quotes
    
    * update footer component.ts
    
    ---------
    
    Co-authored-by: gturner <genevieve.turner@anu.edu.au>

--- src/themes/anu/app/footer/footer.component.ts
+++ src/themes/anu/app/footer/footer.component.ts
@@ -11,7 +11,6 @@ import { environment } from '../../../../environments/environment';
 })
 export class FooterComponent extends BaseComponent {
   showTopFooter = true;
-  
   dataTrackerValue = null;
 
   ngOnInit(){
@@ -21,15 +20,13 @@ export class FooterComponent extends BaseComponent {
   loadscript(){
     const node = document.createElement('script');
     if (environment.production) {
-      node.src = "https://webstyle.anu.edu.au/widgets/bundle.js";
-      this.dataTrackerValue = "anu";
+      node.src = 'https://webstyle.anu.edu.au/widgets/bundle.js';
+      this.dataTrackerValue = 'anu';
     } else {
-      node.src = "https://webstyle-dev.anu.edu.au/widgets/bundle.js";
+      node.src = 'https://webstyle-dev.anu.edu.au/widgets/bundle.js';
     }
     node.type = 'text/javascript';
     node.async = true;
     document.getElementsByTagName('footer')[0].appendChild(node);
-
   }
-
 }
--- src/themes/anu/app/navbar/navbar.component.ts
+++ src/themes/anu/app/navbar/navbar.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { any } from 'prop-types';
+import { find, map } from 'rxjs';
+import { hasValue } from 'src/app/shared/empty.util';
+import { MenuID } from 'src/app/shared/menu/menu-id.model';
+import { MenuItemType } from 'src/app/shared/menu/menu-item-type.model';
+import { LinkMenuItemModel } from 'src/app/shared/menu/menu-item/models/link.model';
+import { TextMenuItemModel } from 'src/app/shared/menu/menu-item/models/text.model';
+import { MenuSection } from 'src/app/shared/menu/menu-section.model';
+import { MenuState } from 'src/app/shared/menu/menu-state.model';
 import { NavbarComponent as BaseComponent } from '../../../../app/navbar/navbar.component';
 import { slideMobileNav } from '../../../../app/shared/animations/slide';
 
@@ -13,5 +22,74 @@ import { slideMobileNav } from '../../../../app/shared/animations/slide';
   templateUrl: '../../../../app/navbar/navbar.component.html',
   animations: [slideMobileNav]
 })
-export class NavbarComponent extends BaseComponent {
+
+
+export class NavbarComponent extends BaseComponent implements OnInit {
+
+  ngOnInit() {
+    super.ngOnInit();
+    this.addToPublicMenu();
+  }
+
+  addToPublicMenu() {
+    const menuList: any[] = [{
+      id: 'related_links_archive',
+      parentID: 'browse_related_links',
+      active: false,
+      visible: true,
+      index: 2,
+      model: {
+        type: MenuItemType.EXTERNAL,
+        text: 'ANU Archives',
+        href: 'https://archives.anu.edu.au/'
+      } as TextMenuItemModel,
+    }, {
+      id: 'related_links_press',
+      parentID: 'browse_related_links',
+      active: false,
+      visible: true,
+      index: 2,
+      model: {
+        type: MenuItemType.EXTERNAL,
+        text: 'ANU Press',
+        href: 'https://press.anu.edu.au/'
+      } as TextMenuItemModel,
+    }, {
+      id: 'related_links_library',
+      parentID: 'browse_related_links',
+      active: false,
+      visible: true,
+      index: 2,
+      model: {
+        type: MenuItemType.EXTERNAL,
+        text: 'ANU Library',
+        href: 'http://anulib.anu.edu.au/'
+      } as TextMenuItemModel,
+    }, {
+      id: 'related_links_rsd',
+      parentID: 'browse_related_links',
+      active: false,
+      visible: true,
+      index: 2,
+      model: {
+        type: MenuItemType.EXTERNAL,
+        text: 'Research Services Division',
+        href: 'https://services.anu.edu.au/business-units/research-services-division'
+      } as TextMenuItemModel,
+    }];
+    menuList.push({
+      id: 'browse_related_links',
+      active: false,
+      visible: true,
+      index: 2,
+      model: {
+        type: MenuItemType.TEXT,
+        text: 'Related Links',
+      } as TextMenuItemModel,
+    });
+
+    menuList.forEach((menuSection) => this.menuService.addSection(MenuID.PUBLIC, Object.assign(menuSection, {
+      shouldPersistOnRouteChange: true
+    })));
+  }
 }
