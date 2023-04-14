1eb8d9dfb4a7a014a2e95a78679c2582866e8fb7
tsakshay <tsakshay@gmail.com>
2023-04-14 13:03:26 +1000

    Update homepage news component and add login or contribute functionality

--- src/themes/anu/app/home-page/home-page.component.ts
+++ src/themes/anu/app/home-page/home-page.component.ts
@@ -9,5 +9,4 @@ import { HomePageComponent as BaseComponent } from '../../../../app/home-page/ho
   //templateUrl: '../../../../app/home-page/home-page.component.html'
 })
 export class HomePageComponent extends BaseComponent {
-
 }
