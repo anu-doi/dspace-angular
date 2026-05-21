2ce18776d26f4abaccdaaa0a045278fcd9c4d9bb
tsakshay <tsakshay@gmail.com>
2026-05-21 15:21:13 +1000

    Add log statements in log-in-external-provider.component to test sso redirect

--- src/app/shared/log-in/methods/log-in-external-provider/log-in-external-provider.component.ts
+++ src/app/shared/log-in/methods/log-in-external-provider/log-in-external-provider.component.ts
@@ -93,17 +93,21 @@ export class LogInExternalProviderComponent implements OnInit {
         redirectRoute = '/';
       }
       const correctRedirectUrl = new URLCombiner(this._window.nativeWindow.origin, redirectRoute).toString();
-
+      console.log("correct Redirect URL : "+correctRedirectUrl);
       let externalServerUrl = this.location;
       const myRegexp = /\?redirectUrl=(.*)/g;
       const match = myRegexp.exec(this.location);
       const redirectUrlFromServer = (match && match[1]) ? match[1] : null;
 
+      console.log("correct Redirect URL : "+correctRedirectUrl);
+      console.log("external Server URL : "+externalServerUrl);
+      console.log('Redirect URL from server : '+redirectUrlFromServer);
       // Check whether the current page is different from the redirect url received from rest
       if (isNotNull(redirectUrlFromServer) && redirectUrlFromServer !== correctRedirectUrl) {
         // change the redirect url with the current page url
         const newRedirectUrl = `?redirectUrl=${correctRedirectUrl}`;
         externalServerUrl = this.location.replace(/\?redirectUrl=(.*)/g, newRedirectUrl);
+        console.log("inside if : "+externalServerUrl);
       }
 
       // redirect to shibboleth authentication url
