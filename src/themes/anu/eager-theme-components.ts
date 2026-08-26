553a17197ede3a39ac186e9a6e050e85aa6fd19a
Genevieve Turner <u5125986@anu.edu.au>
2026-08-26 15:09:35 +1000

    Add base anu theme

---
+++ src/themes/anu/eager-theme-components.ts
@@ -0,0 +1,14 @@
+/* eslint-disable dspace-angular-ts/themed-component-usages */
+import { HeaderComponent } from './app/header/header.component';
+import { HeaderNavbarWrapperComponent } from './app/header-nav-wrapper/header-navbar-wrapper.component';
+// import { HomeNewsComponent } from './app/home-page/home-news/home-news.component';
+import { NavbarComponent } from './app/navbar/navbar.component';
+import { FooterComponent } from './app/footer/footer.component';
+
+export const COMPONENTS = [
+  // HomeNewsComponent,
+  HeaderComponent,
+  HeaderNavbarWrapperComponent,
+  NavbarComponent,
+  FooterComponent,
+];
